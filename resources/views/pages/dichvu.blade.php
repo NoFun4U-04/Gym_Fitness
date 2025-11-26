2a9ba658fcfad435910f00d960c6575928efd18e
Minh <minhhminhh1511@gmail.com>
2025-11-26 18:36:29 +0700

    update

--- resources/views/pages/dichvu.blade.php
+++ resources/views/pages/dichvu.blade.php
@@ -1,3 +1,17 @@
 @push('styles')
-<link rel="stylesheet" href="{{ asset('frontend/css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/dichvu1.css') }}">
 @endpush
+
+<section class="service-banner">
+    <div class="banner-content">
+        <h1>Dịch Vụ Gym Chuyên Nghiệp</h1>
+        <p>
+            Nâng cao sức khỏe và thể chất với các thiết bị hiện đại và đội ngũ
+            huấn luyện viên chuyên nghiệp tại RiseFitness&Yoga.
+        </p>
+
+        <div class="banner-buttons">
+            <a href="{{ url('/register') }}" class="btn-primary">Đăng ký tư vấn</a>
+        </div>
+    </div>
+</section>
