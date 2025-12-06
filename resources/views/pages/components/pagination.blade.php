7dce222203ca0cfd3ea983dfe311bac938cce33e
NoFun4U-04 <ducthinh4129@gmail.com>
2025-12-06 13:56:10 +0700

    fix pagination

--- resources/views/pages/components/pagination.blade.php
+++ resources/views/pages/components/pagination.blade.php
@@ -1,1 +1,11 @@
-
+@if ($products->total() === 0)
+    <p class="text-center mt-4" style="font-weight:600; color:#555;">
+        KHÔNG CÓ SẢN PHẨM PHÙ HỢP
+    </p>
+@else
+    @if ($products->hasPages())
+        <div class="pagination-wrapper">
+            {!! $products->links('pagination::bootstrap-4') !!}
+        </div>
+    @endif
+@endif
